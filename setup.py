5cf8c493fad61940523067fd23540c7a96361ff6
Tom Forbes <tom.forbes@contextis.co.uk>
2016-05-05 14:29:33 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-performance-metrics',
+    version='0.1',
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=[
+    ]
+)
